b285be8b3092bf61599eb119c42d6c33553a26a9
ito-inframe <ito@inframe.jp>
2025-08-15 11:00:17 +0900

    add Routes and setup root page

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>KENSING Demo</h1>
+        <p className="text-muted">The KENSING OCR Demo app</p>
+      </div>
+    </div>
+  );
+}
