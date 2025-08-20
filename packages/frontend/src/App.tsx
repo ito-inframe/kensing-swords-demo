f6a759e58707b21b312fdcd5b1150b0b53c6419f
ito-inframe <ito@inframe.jp>
2025-08-20 15:11:16 +0900

    react-admin fix to show demo

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import { createTheme } from '@material-ui/core/styles';
-import jsonServerProvider from 'ra-data-json-server';
-import simpleRestProvider from 'ra-data-simple-rest';
+import { API, Storage } from "aws-amplify";
+import fakeDataProvider from 'ra-data-fakerest';
 import { MeterList } from './components/meters';
 import './App.css';
+import './containers/Home.css';
+
+const ym = '202506';
 
 // import Dashboard from './dashboard';
 const theme = createTheme({
@@ -16,17 +20,57 @@ const theme = createTheme({
 // データを取得するクライアントとエンドポイントのベースURLの初期化
 
 //const dataProvider = jsonServerProvider('https://xx856cpj6a.execute-api.us-west-2.amazonaws.com');
-const dataProvider = simpleRestProvider('https://xx856cpj6a.execute-api.us-west-2.amazonaws.com');
-
+//const dataProvider = simpleRestProvider('https://xx856cpj6a.execute-api.us-west-2.amazonaws.com');
+let dataProvider = null;
 
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function onLoad() {
+
+      try {
+        const meters = await loadMeters(ym);
+        console.log(meters);
+        const jsonstr = '{' + `"${ym}": ${JSON.stringify(meters)}` + '}';
+        console.log(jsonstr);
+        dataProvider = fakeDataProvider(JSON.parse(jsonstr), true);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    console.log('useEffect called...');
+    onLoad();
+  }, [loading]);
+
+  function loadMeters(ym) {
+    return API.get("kensing", `/meters/${ym}`, {});
+  }
+
+  function renderLander() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>KENSING Demo</h1>
+          <p className="text-muted">The KENSING OCR Demo app</p>
+        </div>
+      </div>
+    );
+  }
+
+  function renderMeters() {
+    return (
+      <Admin theme={theme} dataProvider={dataProvider}>
+        <Resource name={ym} list={MeterList} />
+      </Admin>
+    );
+  }
+
   return (
-    // dataProviderでクライアントを渡す
-    <Admin theme={theme} dataProvider={dataProvider} >
-      <Resource name="meters/202506" list={ListGuesser} />
-    </Admin>
+    <>
+    { loading ? renderLander() :  renderMeters() }
+    </>
   );
 }
 
 export default App;
-
--- packages/frontend/src/components/meters.tsx
+++ packages/frontend/src/components/meters.tsx
@@ -1,6 +1,14 @@
-import { List, Datagrid, TextField } from "react-admin";
+import { List, Datagrid, TextField, ImageField } from "react-admin";
+//import { EditableDatagrid, RowForm } from '@react-admin/ra-editable-datagrid'
 
 export const MeterList = () => (
   <List>
+    <Datagrid rowClick="edit">
+    {/* <EditableDatagrid editForm={<MeterItemForm/>}> */}
+      <TextField label="メーター番号" source="installationMeterNo" />
+      <TextField label="種別" source="typeName" />
+      <TextField label="当月指針" source="presentMonthValue" />
+      <ImageField label="確認画像" source="attachments[0]"  />
+    </Datagrid>
   </List>
 );
